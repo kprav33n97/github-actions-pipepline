daf185a2f559522faa7fc4a6ebe31bba72989617
K Praveen Kumar <kpraveenkumar2006@gmail.com>
2026-09-25 10:37:00 +0530

    Update greeting message to v2

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => res.send('Hello from my CI/CD pipeline!'));
+app.get('/', (req, res) => res.send('Hello from my CI/CD pipeline! v2'));
 
 module.exports = app;
 
