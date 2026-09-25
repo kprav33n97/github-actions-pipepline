dc7fa7bbd69e646a78661700c11cab8bbb793678
K Praveen Kumar <kpraveenkumar2006@gmail.com>
2026-09-25 08:53:35 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => res.send('Hello from my CI/CD pipeline!'));
+
+module.exports = app;
+
+if (require.main === module) {
+  app.listen(3000, () => console.log('Running on port 3000'));
+}
